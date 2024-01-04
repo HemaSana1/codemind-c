323a3f06f9c7bdd3838aed20e2891c4d5cd56cfe
HemaSana1 <147488056+HemaSana1@users.noreply.github.com>
2024-01-04 18:56:12 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='G')     {         printf("Green");     }     else if(ch=='I')     {         printf("Indigo");     }     else if(ch=='B')     {         printf("Blue");     }     else if(ch=='V')     {         printf("Violet");     }     else if(ch=='Y')     {         printf("Yellow");     }     else if(ch=='O') {     printf("Orange"); } else {     printf("-1"); } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='G')
+    {
+        printf("Green");
+    }
+    else if(ch=='I')
+    {
+        printf("Indigo");
+    }
+    else if(ch=='B')
+    {
+        printf("Blue");
+    }
+    else if(ch=='V')
+    {
+        printf("Violet");
+    }
+    else if(ch=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(ch=='O')
+{
+    printf("Orange");
+}
+else
+{
+    printf("-1");
+}
+}
